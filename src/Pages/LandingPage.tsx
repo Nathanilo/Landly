cf883bb792286f2c3c47992fecace7e4c1fb5320
nathanilo <nathanielodion@gmail.com>
2024-09-10 17:35:41 +0100

    Bug fix

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Box } from "@chakra-ui/react";
 import Navbar from "../Components/Navbar";
 import Hero from "../Components/Hero";
@@ -6,7 +7,7 @@ import TopDevelopers from "../Components/TopDevelopers";
 import Testimonials from "../Components/Testimonials";
 import Footer from "../Components/Footer";
 
-const LandingPage = () => {
+const LandingPage:React.FC = () => {
   return (
     <Box>
       <Navbar />
